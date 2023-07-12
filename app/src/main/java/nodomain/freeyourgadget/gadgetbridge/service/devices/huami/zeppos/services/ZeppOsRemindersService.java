zoop-dev/pulse

commit 919eca6173e61ca699eaeb6fd973ca7e83c3051b

Zepp OS: Refactor config, display items, reminders and http to standalone services

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huami/zeppos/services/ZeppOsRemindersService.java
@@ -1,0 +1,267 @@
+/*  Copyright (C) 2023 José Rebelo
+
+    This file is part of Gadgetbridge.
+
+    Gadgetbridge is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gadgetbridge is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+package nodomain.freeyourgadget.gadgetbridge.service.devices.huami.zeppos.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import nodomain.freeyourgadget.gadgetbridge.database.DBHelper;
+import nodomain.freeyourgadget.gadgetbridge.deviceevents.GBDeviceEventUpdatePreferences;
+import nodomain.freeyourgadget.gadgetbridge.devices.DeviceCoordinator;
+import nodomain.freeyourgadget.gadgetbridge.model.Reminder;
+import nodomain.freeyourgadget.gadgetbridge.service.btle.BLETypeConversions;
+import nodomain.freeyourgadget.gadgetbridge.service.btle.TransactionBuilder;
+import nodomain.freeyourgadget.gadgetbridge.service.devices.huami.Huami2021Support;
+import nodomain.freeyourgadget.gadgetbridge.service.devices.huami.zeppos.AbstractZeppOsService;
+import nodomain.freeyourgadget.gadgetbridge.util.Prefs;
+import nodomain.freeyourgadget.gadgetbridge.util.StringUtils;
+
+public class ZeppOsRemindersService extends AbstractZeppOsService {
+    private static final Logger LOG = LoggerFactory.getLogger(ZeppOsRemindersService.class);
+
+    private static final short ENDPOINT = 0x0038;
+
+    private static final byte CMD_CAPABILITIES_REQUEST = 0x01;
+    private static final byte CMD_CAPABILITIES_RESPONSE = 0x02;
+    private static final byte CMD_REQUEST = 0x03;
+    private static final byte CMD_RESPONSE = 0x04;
+    private static final byte CMD_CREATE = 0x05;
+    private static final byte CMD_CREATE_ACK = 0x06;
+    private static final byte CMD_UPDATE = 0x07;
+    private static final byte CMD_UPDATE_ACK = 0x08;
+    private static final byte CMD_DELETE = 0x09;
+    private static final byte CMD_DELETE_ACK = 0x0a;
+
+    private static final int FLAG_ENABLED = 0x0001;
+    private static final int FLAG_TEXT = 0x0008;
+    private static final int FLAG_REPEAT_MONTH = 0x1000;
+    private static final int FLAG_REPEAT_YEAR = 0x2000;
+
+    private static final String PREF_CAPABILITY = "huami_2021_capability_reminders";
+
+    public ZeppOsRemindersService(final Huami2021Support support) {
+        super(support);
+    }
+
+    @Override
+    public short getEndpoint() {
+        return ENDPOINT;
+    }
+
+    @Override
+    public boolean isEncrypted() {
+        return false;
+    }
+
+    @Override
+    public void handlePayload(final byte[] payload) {
+        switch (payload[0]) {
+            case CMD_CAPABILITIES_RESPONSE:
+                LOG.info("Reminder capability, version = {}", payload[1]);
+                if (payload[1] != 1) {
+                    LOG.warn("Reminder unsupported version {}", payload[1]);
+                    return;
+                }
+                final int numReminders = payload[2] & 0xff;
+                final GBDeviceEventUpdatePreferences eventUpdatePreferences = new GBDeviceEventUpdatePreferences(
+                        PREF_CAPABILITY,
+                        numReminders
+                );
+                evaluateGBDeviceEvent(eventUpdatePreferences);
+                return;
+            case CMD_CREATE_ACK:
+                LOG.info("Reminder create ACK, status = {}", payload[1]);
+                return;
+            case CMD_DELETE_ACK:
+                LOG.info("Reminder delete ACK, status = {}", payload[1]);
+                // status 1 = success
+                // status 2 = reminder not found
+                return;
+            case CMD_UPDATE_ACK:
+                LOG.info("Reminder update ACK, status = {}", payload[1]);
+                return;
+            case CMD_RESPONSE:
+                LOG.info("Got reminders from band");
+                decodeAndUpdateReminders(payload);
+                return;
+            default:
+                LOG.warn("Unexpected reminders payload byte {}", String.format("0x%02x", payload[0]));
+        }
+    }
+
+    @Override
+    public void initialize(final TransactionBuilder builder) {
+        requestCapabilities(builder);
+        //requestReminders(builder);
+        sendReminders(builder);
+    }
+
+    private void requestCapabilities(final TransactionBuilder builder) {
+        write(builder, CMD_CAPABILITIES_REQUEST);
+    }
+
+    private void requestReminders(final TransactionBuilder builder) {
+        write(builder, CMD_REQUEST);
+    }
+
+    public void sendReminders(final TransactionBuilder builder) {
+        final List<? extends Reminder> reminders = DBHelper.getReminders(getSupport().getDevice());
+        sendReminders(builder, reminders);
+    }
+
+    public void sendReminders(final TransactionBuilder builder, final List<? extends Reminder> reminders) {
+        LOG.info("On Set Reminders: {}", reminders.size());
+
+        final int reminderSlotCount = getCoordinator().getReminderSlotCount(getSupport().getDevice());
+        if (reminderSlotCount <= 0) {
+            LOG.warn("Reminders not yet initialized");
+            return;
+        }
+
+        // Send the reminders, skipping the reserved slots for calendar events
+        for (int i = 0; i < reminders.size(); i++) {
+            LOG.debug("Sending reminder at position {}", i);
+        }
+
+        // Delete the remaining slots, skipping the sent reminders
+        for (int i = reminders.size(); i < reminderSlotCount; i++) {
+            LOG.debug("Deleting reminder at position {}", i);
+
+            sendReminderToDevice(builder, i, null);
+        }
+    }
+
+    protected void sendReminderToDevice(final TransactionBuilder builder, int position, final Reminder reminder) {
+        final DeviceCoordinator coordinator = getCoordinator();
+        final int reminderSlotCount = coordinator.getReminderSlotCount(getSupport().getDevice());
+        if (position + 1 > reminderSlotCount) {
+            LOG.error("Reminder for position {} is over the limit of {} reminders", position, reminderSlotCount);
+            return;
+        }
+
+        if (reminder == null) {
+            // Delete reminder
+            write(builder, new byte[]{CMD_DELETE, (byte) (position & 0xFF)});
+
+            return;
+        }
+
+        final String message;
+        if (reminder.getMessage().length() > coordinator.getMaximumReminderMessageLength()) {
+            LOG.warn("The reminder message length {} is longer than {}, will be truncated",
+                    reminder.getMessage().length(),
+                    coordinator.getMaximumReminderMessageLength()
+            );
+            message = StringUtils.truncate(reminder.getMessage(), coordinator.getMaximumReminderMessageLength());
+        } else {
+            message = reminder.getMessage();
+        }
+
+        final ByteBuffer buf = ByteBuffer.allocate(1 + 10 + message.getBytes(StandardCharsets.UTF_8).length + 1);
+        buf.order(ByteOrder.LITTLE_ENDIAN);
+
+        // Update does an upsert, so let's use it. If we call create twice on the same ID, it becomes weird
+        buf.put(CMD_UPDATE);
+        buf.put((byte) (position & 0xFF));
+
+        final Calendar cal = BLETypeConversions.createCalendar();
+        cal.setTime(reminder.getDate());
+
+        int reminderFlags = FLAG_ENABLED | FLAG_TEXT;
+
+        switch (reminder.getRepetition()) {
+            case Reminder.ONCE:
+                // Default is once, nothing to do
+                break;
+            case Reminder.EVERY_DAY:
+                reminderFlags |= 0x0fe0; // all week day bits set
+                break;
+            case Reminder.EVERY_WEEK:
+                int dayOfWeek = BLETypeConversions.dayOfWeekToRawBytes(cal) - 1; // Monday = 0
+                reminderFlags |= 0x20 << dayOfWeek;
+                break;
+            case Reminder.EVERY_MONTH:
+                reminderFlags |= FLAG_REPEAT_MONTH;
+                break;
+            case Reminder.EVERY_YEAR:
+                reminderFlags |= FLAG_REPEAT_YEAR;
+                break;
+            default:
+                LOG.warn("Unknown repetition for reminder in position {}, defaulting to once", position);
+        }
+
+        buf.putInt(reminderFlags);
+
+        buf.putInt((int) (cal.getTimeInMillis() / 1000L));
+        buf.put((byte) 0x00);
+
+        buf.put(message.getBytes(StandardCharsets.UTF_8));
+        buf.put((byte) 0x00);
+
+        write(builder, buf.array());
+    }
+
+    private void decodeAndUpdateReminders(final byte[] payload) {
+        final int numReminders = payload[1];
+
+        if (payload.length < 3 + numReminders * 11) {
+            LOG.warn("Unexpected payload length of {} for {} reminders", payload.length, numReminders);
+            return;
+        }
+
+        int i = 3;
+        while (i < payload.length) {
+            if (payload.length - i < 11) {
+                LOG.error("Not enough bytes remaining to parse a reminder ({})", payload.length - i);
+                return;
+            }
+
+            final int reminderPosition = payload[i++] & 0xff;
+            final int reminderFlags = BLETypeConversions.toUint32(payload, i);
+            i += 4;
+            final int reminderTimestamp = BLETypeConversions.toUint32(payload, i);
+            i += 4;
+            i++; // 0 ?
+            final Date reminderDate = new Date(reminderTimestamp * 1000L);
+            final String reminderText = StringUtils.untilNullTerminator(payload, i);
+            if (reminderText == null) {
+                LOG.error("Failed to parse reminder text at pos {}", i);
+                return;
+            }
+
+            i += reminderText.length() + 1;
+
+            LOG.info("Reminder {}, {}, {}, {}", reminderPosition, String.format("0x%04x", reminderFlags), reminderDate, reminderText);
+        }
+        if (i != payload.length) {
+            LOG.error("Unexpected reminders payload trailer, {} bytes were not consumed", payload.length - i);
+        }
+
+        // TODO persist in database. Probably not trivial, because reminderPosition != reminderId
+    }
+
+    public static int getSlotCount(final Prefs devicePrefs) {
+        return devicePrefs.getInt(PREF_CAPABILITY, 0);
+    }
+}

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/huami/Huami2021Coordinator.java
@@ -52,6 +52,7 @@ import nodomain.freeyourgadget.gadgetbridge.service.devices.huami.AbstractHuami2
 import nodomain.freeyourgadget.gadgetbridge.service.devices.huami.zeppos.services.ZeppOsAlexaService;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.huami.zeppos.services.ZeppOsContactsService;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.huami.zeppos.services.ZeppOsLogsService;
+import nodomain.freeyourgadget.gadgetbridge.service.devices.huami.zeppos.services.ZeppOsRemindersService;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.huami.zeppos.services.ZeppOsShortcutCardsService;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.huami.zeppos.services.ZeppOsConfigService;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.huami.HuamiLanguageType;
@@ -241,7 +242,7 @@ public abstract class Huami2021Coordinator extends HuamiCoordinator {
 
     @Override
     public int getReminderSlotCount(final GBDevice device) {
-        return getPrefs(device).getInt(Huami2021Service.REMINDERS_PREF_CAPABILITY, 0);
+        return ZeppOsRemindersService.getSlotCount(getPrefs(device));
     }
 
     @Override

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/huami/Huami2021Service.java
@@ -20,7 +20,6 @@ public class Huami2021Service {
     /**
      * Endpoints for 2021 chunked protocol
      */
-    public static final short CHUNKED2021_ENDPOINT_HTTP = 0x0001;
     public static final short CHUNKED2021_ENDPOINT_WEATHER = 0x000e;
     public static final short CHUNKED2021_ENDPOINT_CONNECTION = 0x0015;
     public static final short CHUNKED2021_ENDPOINT_USER_INFO = 0x0017;
@@ -30,36 +29,10 @@ public class Huami2021Service {
     public static final short CHUNKED2021_ENDPOINT_FIND_DEVICE = 0x001a;
     public static final short CHUNKED2021_ENDPOINT_MUSIC = 0x001b;
     public static final short CHUNKED2021_ENDPOINT_HEARTRATE = 0x001d;
-    public static final short CHUNKED2021_ENDPOINT_DISPLAY_ITEMS = 0x0026;
     public static final short CHUNKED2021_ENDPOINT_BATTERY = 0x0029;
-    public static final short CHUNKED2021_ENDPOINT_REMINDERS = 0x0038;
     public static final short CHUNKED2021_ENDPOINT_SILENT_MODE = 0x003b;
     public static final short CHUNKED2021_ENDPOINT_AUTH = 0x0082;
     public static final short CHUNKED2021_ENDPOINT_COMPAT = 0x0090;
-
-    /**
-     * HTTP, for {@link Huami2021Service#CHUNKED2021_ENDPOINT_HTTP}.
-     */
-    public static final byte HTTP_CMD_REQUEST = 0x01;
-    public static final byte HTTP_CMD_RESPONSE = 0x02;
-    public static final byte HTTP_RESPONSE_SUCCESS = 0x01;
-    public static final byte HTTP_RESPONSE_NO_INTERNET = 0x02;
-
-    /**
-     * Display Items, for {@link Huami2021Service#CHUNKED2021_ENDPOINT_DISPLAY_ITEMS}.
-     */
-    public static final byte DISPLAY_ITEMS_CMD_CAPABILITIES_REQUEST = 0x01;
-    public static final byte DISPLAY_ITEMS_CMD_CAPABILITIES_RESPONSE = 0x02;
-    public static final byte DISPLAY_ITEMS_CMD_REQUEST = 0x03;
-    public static final byte DISPLAY_ITEMS_CMD_RESPONSE = 0x04;
-    public static final byte DISPLAY_ITEMS_CMD_CREATE = 0x05;
-    public static final byte DISPLAY_ITEMS_CMD_CREATE_ACK = 0x06;
-    public static final byte DISPLAY_ITEMS_MENU = 0x01;
-    public static final byte DISPLAY_ITEMS_SHORTCUTS = 0x02;
-    public static final byte DISPLAY_ITEMS_CONTROL_CENTER = 0x03;
-    public static final byte DISPLAY_ITEMS_SECTION_MAIN = 0x01;
-    public static final byte DISPLAY_ITEMS_SECTION_MORE = 0x02;
-    public static final byte DISPLAY_ITEMS_SECTION_DISABLED = 0x03;
 
     /**
      * Find Device, for {@link Huami2021Service#CHUNKED2021_ENDPOINT_FIND_DEVICE}.
@@ -156,25 +129,6 @@ public class Huami2021Service {
     public static final byte MUSIC_BUTTON_PREVIOUS = 0x04;
     public static final byte MUSIC_BUTTON_VOLUME_UP = 0x05;
     public static final byte MUSIC_BUTTON_VOLUME_DOWN = 0x06;
-
-    /**
-     * Reminders, for {@link Huami2021Service#CHUNKED2021_ENDPOINT_REMINDERS}.
-     */
-    public static final byte REMINDERS_CMD_CAPABILITIES_REQUEST = 0x01;
-    public static final byte REMINDERS_CMD_CAPABILITIES_RESPONSE = 0x02;
-    public static final byte REMINDERS_CMD_REQUEST = 0x03;
-    public static final byte REMINDERS_CMD_RESPONSE = 0x04;
-    public static final byte REMINDERS_CMD_CREATE = 0x05;
-    public static final byte REMINDERS_CMD_CREATE_ACK = 0x06;
-    public static final byte REMINDERS_CMD_UPDATE = 0x07;
-    public static final byte REMINDERS_CMD_UPDATE_ACK = 0x08;
-    public static final byte REMINDERS_CMD_DELETE = 0x09;
-    public static final byte REMINDERS_CMD_DELETE_ACK = 0x0a;
-    public static final int REMINDER_FLAG_ENABLED = 0x0001;
-    public static final int REMINDER_FLAG_TEXT = 0x0008;
-    public static final int REMINDER_FLAG_REPEAT_MONTH = 0x1000;
-    public static final int REMINDER_FLAG_REPEAT_YEAR = 0x2000;
-    public static final String REMINDERS_PREF_CAPABILITY = "huami_2021_capability_reminders";
 
     /**
      * Weather, for {@link Huami2021Service#CHUNKED2021_ENDPOINT_WEATHER}.

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huami/HuamiSupport.java
@@ -1009,7 +1009,7 @@ public abstract class HuamiSupport extends AbstractBTLEDeviceSupport implements 
         sendReminders(builder, reminders);
     }
 
-    protected void sendReminders(final TransactionBuilder builder, final List<? extends Reminder> reminders) {
+    private void sendReminders(final TransactionBuilder builder, final List<? extends Reminder> reminders) {
         final DeviceCoordinator coordinator = DeviceHelper.getInstance().getCoordinator(gbDevice);
 
         final Prefs prefs = new Prefs(GBApplication.getDeviceSpecificSharedPrefs(gbDevice.getAddress()));
@@ -1032,7 +1032,7 @@ public abstract class HuamiSupport extends AbstractBTLEDeviceSupport implements 
         }
     }
 
-    protected void sendReminderToDevice(final TransactionBuilder builder, int position, final Reminder reminder) {
+    private void sendReminderToDevice(final TransactionBuilder builder, int position, final Reminder reminder) {
         if (characteristicChunked == null) {
             LOG.warn("characteristicChunked is null, not sending reminder");
             return;
@@ -4082,11 +4082,6 @@ public abstract class HuamiSupport extends AbstractBTLEDeviceSupport implements 
         //FIXME: on older devices only the first one works, and on newer only the last is sufficient
         writeToConfiguration(builder, HuamiService.COMMAND_REQUEST_ALARMS);
         writeToConfiguration(builder, HuamiService.COMMAND_REQUEST_ALARMS_WITH_TIMES);
-        return this;
-    }
-
-    public HuamiSupport requestDisplayItems(TransactionBuilder builder) {
-        LOG.warn("Function not implemented");
         return this;
     }
 

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huami/zeppos/AbstractZeppOsService.java
@@ -18,9 +18,13 @@ package nodomain.freeyourgadget.gadgetbridge.service.devices.huami.zeppos;
 
 import android.content.Context;
 
+import nodomain.freeyourgadget.gadgetbridge.GBApplication;
 import nodomain.freeyourgadget.gadgetbridge.deviceevents.GBDeviceEvent;
+import nodomain.freeyourgadget.gadgetbridge.devices.DeviceCoordinator;
+import nodomain.freeyourgadget.gadgetbridge.devices.huami.Huami2021Coordinator;
 import nodomain.freeyourgadget.gadgetbridge.service.btle.TransactionBuilder;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.huami.Huami2021Support;
+import nodomain.freeyourgadget.gadgetbridge.util.DeviceHelper;
 import nodomain.freeyourgadget.gadgetbridge.util.Prefs;
 
 public abstract class AbstractZeppOsService {
@@ -52,6 +56,15 @@ public abstract class AbstractZeppOsService {
 
     protected Huami2021Support getSupport() {
         return mSupport;
+    }
+
+    protected Huami2021Coordinator getCoordinator() {
+        final DeviceCoordinator coordinator = DeviceHelper.getInstance().getCoordinator(getSupport().getDevice());
+        return (Huami2021Coordinator) coordinator;
+    }
+
+    protected Prefs getDevicePrefs() {
+        return new Prefs(GBApplication.getDeviceSpecificSharedPrefs(getSupport().getDevice().getAddress()));
     }
 
     protected void write(final String taskName, final byte b) {

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huami/zeppos/services/ZeppOsConfigService.java
@@ -25,6 +25,8 @@ import static nodomain.freeyourgadget.gadgetbridge.devices.miband.MiBandConst.PR
 import static nodomain.freeyourgadget.gadgetbridge.devices.miband.MiBandConst.PREF_NIGHT_MODE_END;
 import static nodomain.freeyourgadget.gadgetbridge.devices.miband.MiBandConst.PREF_NIGHT_MODE_START;
 
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 
 import org.apache.commons.lang3.ArrayUtils;
@@ -124,6 +126,34 @@ public class ZeppOsConfigService extends AbstractZeppOsService {
             default:
                 LOG.warn("Unexpected configuration payload byte {}", String.format("0x%02x", payload[0]));
         }
+    }
+
+    @Override
+    public void initialize(TransactionBuilder builder) {
+        requestAllConfigs(builder);
+    }
+
+    @Override
+    public boolean onSendConfiguration(final String prefKey, Prefs prefs) {
+        if (!PREF_TO_CONFIG.containsKey(prefKey)) {
+            return false;
+        }
+
+        final ConfigSetter configSetter = new ConfigSetter();
+        if (setConfig(prefs, prefKey, configSetter)) {
+            try {
+                // If the ConfigSetter was able to set the config, just write it and return
+                final TransactionBuilder builder = new TransactionBuilder("Sending configuration for " + prefKey);
+                configSetter.write(builder);
+                builder.queue(getSupport().getQueue());
+            } catch (final Exception e) {
+                GB.toast("Error setting configuration", Toast.LENGTH_LONG, GB.ERROR, e);
+            }
+
+            return true;
+        }
+
+        return false;
     }
 
     private boolean sentFitnessGoal = false;

commit a0ba6dd2f090d484767736a73c3ffff6c4fb1840

Zepp OS: Fix reminder creation
Regression introduced by 919eca6173e61ca699eaeb6fd973ca7e83c3051b

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/huami/zeppos/services/ZeppOsRemindersService.java
@@ -139,9 +139,11 @@ public class ZeppOsRemindersService extends AbstractZeppOsService {
             return;
         }
 
-        // Send the reminders, skipping the reserved slots for calendar events
+        // Send the reminders
         for (int i = 0; i < reminders.size(); i++) {
             LOG.debug("Sending reminder at position {}", i);
+
+            sendReminderToDevice(builder, i, reminders.get(i));
         }
 
         // Delete the remaining slots, skipping the sent reminders
@@ -223,12 +225,14 @@ public class ZeppOsRemindersService extends AbstractZeppOsService {
     }
 
     private void decodeAndUpdateReminders(final byte[] payload) {
-        final int numReminders = payload[1];
+        final int numReminders = payload[1] & 0xff;
 
         if (payload.length < 3 + numReminders * 11) {
             LOG.warn("Unexpected payload length of {} for {} reminders", payload.length, numReminders);
             return;
         }
+
+        LOG.debug("Got {} reminders from band", numReminders);
 
         int i = 3;
         while (i < payload.length) {
@@ -252,7 +256,7 @@ public class ZeppOsRemindersService extends AbstractZeppOsService {
 
             i += reminderText.length() + 1;
 
-            LOG.info("Reminder {}, {}, {}, {}", reminderPosition, String.format("0x%04x", reminderFlags), reminderDate, reminderText);
+            LOG.info("Reminder[{}]: {}, {}, {}", reminderPosition, String.format("0x%04x", reminderFlags), reminderDate, reminderText);
         }
         if (i != payload.length) {
             LOG.error("Unexpected reminders payload trailer, {} bytes were not consumed", payload.length - i);