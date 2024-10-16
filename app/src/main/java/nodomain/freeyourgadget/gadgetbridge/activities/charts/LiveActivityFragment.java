zoop-dev/pulse

commit cdff4a997cfca5531b332c7866ec0f9b9e5ea9b7

Limit LiveActivityFragment to just the current device

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/charts/LiveActivityFragment.java
@@ -348,7 +348,7 @@ public class LiveActivityFragment extends AbstractActivityChartFragment<ChartsDa
         renderCharts();
 
         // have to enable it again and again to keep it measuring
-        GBApplication.deviceService().onEnableRealtimeHeartRateMeasurement(true);
+        GBApplication.deviceService(getChartsHost().getDevice()).onEnableRealtimeHeartRateMeasurement(true);
     }
 
     private int getPulseIntervalMillis() {
@@ -367,8 +367,8 @@ public class LiveActivityFragment extends AbstractActivityChartFragment<ChartsDa
             return;
         }
 
-        GBApplication.deviceService().onEnableRealtimeSteps(enable);
-        GBApplication.deviceService().onEnableRealtimeHeartRateMeasurement(enable);
+        GBApplication.deviceService(getChartsHost().getDevice()).onEnableRealtimeSteps(enable);
+        GBApplication.deviceService(getChartsHost().getDevice()).onEnableRealtimeHeartRateMeasurement(enable);
         if (enable) {
             if (getActivity() != null) {
                 getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);

commit 8255f244acf690a24d3b2b534c46ac5eb4728546

Fix crash in LiveActivityFragment that was introduced by cdff4a997

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/charts/LiveActivityFragment.java
@@ -367,8 +367,15 @@ public class LiveActivityFragment extends AbstractActivityChartFragment<ChartsDa
             return;
         }
 
-        GBApplication.deviceService(getChartsHost().getDevice()).onEnableRealtimeSteps(enable);
-        GBApplication.deviceService(getChartsHost().getDevice()).onEnableRealtimeHeartRateMeasurement(enable);
+        try {
+            GBApplication.deviceService(getChartsHost().getDevice()).onEnableRealtimeSteps(enable);
+            GBApplication.deviceService(getChartsHost().getDevice()).onEnableRealtimeHeartRateMeasurement(enable);
+        } catch (IllegalStateException e) {
+            LOG.error("IllegalStateException catched, setting realtime tracking globally to {}", enable);
+            GBApplication.deviceService().onEnableRealtimeSteps(enable);
+            GBApplication.deviceService().onEnableRealtimeHeartRateMeasurement(enable);
+        }
+
         if (enable) {
             if (getActivity() != null) {
                 getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);