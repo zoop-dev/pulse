6fc207cb0c60d17adb1c21cc2850f61dda5da66c
José Rebelo <joserebelo@outlook.com>
2025-01-22 21:37:16 +0000

    SuperCars: Adopt device card custom action

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/supercars/SuperCarsCoordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/supercars/SuperCarsCoordinator.java
@@ -17,38 +17,28 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 package nodomain.freeyourgadget.gadgetbridge.devices.supercars;
 
-import android.app.Activity;
 import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import nodomain.freeyourgadget.gadgetbridge.GBException;
 import nodomain.freeyourgadget.gadgetbridge.R;
 import nodomain.freeyourgadget.gadgetbridge.devices.AbstractDeviceCoordinator;
-import nodomain.freeyourgadget.gadgetbridge.devices.InstallHandler;
-import nodomain.freeyourgadget.gadgetbridge.devices.SampleProvider;
+import nodomain.freeyourgadget.gadgetbridge.devices.DeviceCardAction;
 import nodomain.freeyourgadget.gadgetbridge.entities.DaoSession;
 import nodomain.freeyourgadget.gadgetbridge.entities.Device;
 import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
-import nodomain.freeyourgadget.gadgetbridge.impl.GBDeviceCandidate;
-import nodomain.freeyourgadget.gadgetbridge.model.ActivitySample;
-import nodomain.freeyourgadget.gadgetbridge.model.DeviceType;
 import nodomain.freeyourgadget.gadgetbridge.service.DeviceSupport;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.supercars.SuperCarsSupport;
 
 public class SuperCarsCoordinator extends AbstractDeviceCoordinator {
-    private static final Logger LOG = LoggerFactory.getLogger(SuperCarsCoordinator.class);
-
     @Override
     protected void deleteDevice(@NonNull GBDevice gbDevice, @NonNull Device device, @NonNull DaoSession session) throws GBException {
-
     }
 
     @Override
@@ -61,56 +51,10 @@ public class SuperCarsCoordinator extends AbstractDeviceCoordinator {
         return BONDING_STYLE_NONE;
     }
 
-    @Override
-    public int getBatteryCount(final GBDevice device) {
-        return 1;
-    }
-
     @NonNull
     @Override
     public Class<? extends DeviceSupport> getDeviceSupportClass() {
         return SuperCarsSupport.class;
-    }
-
-    @Override
-    public Class<? extends Activity> getAppsManagementActivity() {
-        return ControlActivity.class;
-    }
-
-    @Nullable
-    @Override
-    public Class<? extends Activity> getPairingActivity() {
-        return null;
-    }
-
-    @Override
-    public boolean supportsActivityDataFetching() {
-        return false;
-    }
-
-    @Override
-    public boolean supportsActivityTracking() {
-        return false;
-    }
-
-    @Override
-    public SampleProvider<? extends ActivitySample> getSampleProvider(GBDevice device, DaoSession session) {
-        return null;
-    }
-
-    @Override
-    public InstallHandler findInstallHandler(Uri uri, Context context) {
-        return null;
-    }
-
-    @Override
-    public boolean supportsScreenshots(final GBDevice device) {
-        return false;
-    }
-
-    @Override
-    public int getAlarmSlotCount(GBDevice device) {
-        return 0;
     }
 
     @Override
@@ -119,31 +63,9 @@ public class SuperCarsCoordinator extends AbstractDeviceCoordinator {
     }
 
     @Override
-    public boolean supportsAppsManagement(final GBDevice device) {
-        return true;
-    }
-
-    @Override
-    public boolean supportsCalendarEvents() {
-        return false;
-    }
-
-    @Override
-    public boolean supportsRealtimeData() {
-        return false;
-    }
-
-    @Override
-    public boolean supportsFindDevice() {
-        return false;
-    }
-
-
-    @Override
     public int getDeviceNameResource() {
         return R.string.devicetype_super_cars;
     }
-
 
     @Override
     public int getDefaultIconResource() {
@@ -153,5 +75,29 @@ public class SuperCarsCoordinator extends AbstractDeviceCoordinator {
     @Override
     public int getDisabledIconResource() {
         return R.drawable.ic_device_supercars_disabled;
+    }
+
+    @Override
+    public List<DeviceCardAction> getCustomActions() {
+        return Collections.singletonList(new ControlDeviceCardAction());
+    }
+
+    private static final class ControlDeviceCardAction implements DeviceCardAction {
+        @Override
+        public int getIcon(GBDevice device) {
+            return R.drawable.ic_steering_wheel;
+        }
+
+        @Override
+        public String getDescription(final GBDevice device, final Context context) {
+            return context.getString(R.string.remote_control);
+        }
+
+        @Override
+        public void onClick(final GBDevice device, final Context context) {
+            final Intent startIntent = new Intent(context, ControlActivity.class);
+            startIntent.putExtra(GBDevice.EXTRA_DEVICE, device);
+            context.startActivity(startIntent);
+        }
     }
 }
