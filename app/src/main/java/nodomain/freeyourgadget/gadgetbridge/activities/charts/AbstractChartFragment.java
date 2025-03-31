zoop-dev/pulse

commit 3977774fb2c47d363b7489afbcaca72728a2dc67

Prevent crash when AbstractActivityChartFragment is wrongly extended
Some fragments are extending AbstractActivityChartFragment when they should
not, since they are not in an instance of ChartsHost. This will make them
crash occasionally when switching dates in the actual charts. Prevent this,
but log at error level.

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/charts/AbstractChartFragment.java
@@ -205,6 +205,12 @@ public abstract class AbstractChartFragment<D extends ChartsData> extends Abstra
     }
 
     protected void onReceive(Context context, Intent intent) {
+        final FragmentActivity fragmentActivity = requireActivity();
+        if (fragmentActivity instanceof ChartsHost) {
+            LOG.error("{} is not an instance of ChartsHost, preventing crash", fragmentActivity.getClass());
+            return;
+        }
+
         String action = intent.getAction();
         if (ChartsHost.REFRESH.equals(action)) {
             updateDateInfo(getStartDate(), getEndDate());

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/ActivitySummariesChartFragment.java
@@ -17,6 +17,7 @@
 package nodomain.freeyourgadget.gadgetbridge.activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -72,6 +73,18 @@ public class ActivitySummariesChartFragment extends AbstractActivityChartFragmen
     private GBDevice gbDevice;
     private int startTime;
     private int endTime;
+
+    @Override
+    protected void onReceive(final Context context, final Intent intent) {
+        // FIXME: We need to override this, or we crash
+        //  This class should be refactored not to extend AbstractActivityChartFragment
+        //    java.lang.ClassCastException: nodomain.freeyourgadget.gadgetbridge.activities.ActivitySummaryDetail cannot be cast to nodomain.freeyourgadget.gadgetbridge.activities.charts.ChartsHost
+        //      at nodomain.freeyourgadget.gadgetbridge.activities.charts.AbstractChartFragment.getChartsHost(AbstractChartFragment.java:164)
+        //      at nodomain.freeyourgadget.gadgetbridge.activities.charts.AbstractChartFragment.getStartDate(AbstractChartFragment.java:176)
+        //      at nodomain.freeyourgadget.gadgetbridge.activities.charts.AbstractChartFragment.onReceive(AbstractChartFragment.java:215)
+        //      at nodomain.freeyourgadget.gadgetbridge.activities.charts.AbstractChartFragment$1.onReceive(AbstractChartFragment.java:82)
+        //      at androidx.localbroadcastmanager.content.LocalBroadcastManager.executePendingBroadcasts(LocalBroadcastManager.java:319)
+    }
 
     public void setDateAndGetData(@Nullable File trackFile, GBDevice gbDevice, long startTime, long endTime) {
         this.trackFile = trackFile;

commit b68e623f691f982ba184441d6a149a0538ed3e3c

Fix day switch in activity charts
Regression introduced by 3977774fb

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/charts/AbstractChartFragment.java
@@ -206,7 +206,7 @@ public abstract class AbstractChartFragment<D extends ChartsData> extends Abstra
 
     protected void onReceive(Context context, Intent intent) {
         final FragmentActivity fragmentActivity = requireActivity();
-        if (fragmentActivity instanceof ChartsHost) {
+        if (!(fragmentActivity instanceof ChartsHost)) {
             LOG.error("{} is not an instance of ChartsHost, preventing crash", fragmentActivity.getClass());
             return;
         }