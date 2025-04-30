d0b6e7da1f04250ec692b90fd6bd66e9808545a1
José Rebelo <joserebelo@outlook.com>
2025-04-30 12:55:03 +0100

    Garmin: Do not overwrite activity name when updating summary

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/garmin/GarminWorkoutParser.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/garmin/GarminWorkoutParser.java
@@ -41,6 +41,7 @@ import nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.fit.messages.
 import nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.fit.messages.FitTimeInZone;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.fit.messages.FitUserProfile;
 import nodomain.freeyourgadget.gadgetbridge.util.FileUtils;
+import nodomain.freeyourgadget.gadgetbridge.util.StringUtils;
 
 public class GarminWorkoutParser implements ActivitySummaryParser {
     private static final Logger LOG = LoggerFactory.getLogger(GarminWorkoutParser.class);
@@ -170,7 +171,9 @@ public class GarminWorkoutParser implements ActivitySummaryParser {
 
         final ActivityKind activityKind;
         if (sport != null) {
-            summary.setName(sport.getName());
+            if (StringUtils.isNullOrEmpty(summary.getName())) {
+                summary.setName(sport.getName());
+            }
             activityKind = getActivityKind(sport.getSport(), sport.getSubSport());
         } else {
             activityKind = getActivityKind(session.getSport(), session.getSubSport());
