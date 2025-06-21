ffc0df53d8afe430c4e50b112811f0a50103fdf9
José Rebelo <joserebelo@outlook.com>
2025-06-21 10:05:15 +0100

    AAWireless: Add car device icon

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/aawireless/AAWirelessCoordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/aawireless/AAWirelessCoordinator.java
@@ -74,6 +74,16 @@ public class AAWirelessCoordinator extends AbstractBLClassicDeviceCoordinator {
     }
 
     @Override
+    public int getDefaultIconResource() {
+        return R.drawable.ic_device_car;
+    }
+
+    @Override
+    public int getDisabledIconResource() {
+        return R.drawable.ic_device_car_disabled;
+    }
+
+    @Override
     public int getBatteryCount(final GBDevice device) {
         return 0;
     }
