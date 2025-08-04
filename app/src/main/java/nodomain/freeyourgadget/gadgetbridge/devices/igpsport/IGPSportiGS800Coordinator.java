7cd2f9f624537cb44eda376d3ac2c27eb961beaf
Vitaliy Tomin <highwaystar.ru@gmail.com>
2025-08-04 15:43:29 +0800

    igpsport: Add iGS800

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/igpsport/IGPSportiGS800Coordinator.java
@@ -0,0 +1,44 @@
+/*  Copyright (C) 2025 Vitaliy Tomin, Thomas Kuehne
+
+    This file is part of Gadgetbridge.
+
+    Gadgetbridge is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gadgetbridge is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+package nodomain.freeyourgadget.gadgetbridge.devices.igpsport;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.regex.Pattern;
+
+import nodomain.freeyourgadget.gadgetbridge.R;
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
+
+public class IGPSportiGS800Coordinator extends IGPSportAbstractCoordinator {
+    private static final Logger LOG = LoggerFactory.getLogger(IGPSportiGS800Coordinator.class);
+
+    @Override
+    public int getDeviceNameResource() {
+        return R.string.devicetype_igpsport_igs800;
+    }
+
+    @Override
+    protected Pattern getSupportedDeviceName() {
+        return Pattern.compile("iGS800");
+    }
+
+    @Override
+    public boolean supportsWeather(final GBDevice device) {
+        return true;
+    }
+}
--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/DeviceType.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/DeviceType.java
@@ -318,6 +318,7 @@ import nodomain.freeyourgadget.gadgetbridge.devices.id115.ID115Coordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.idasen.IdasenCoordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.igpsport.IGPSportBSC200Coordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.igpsport.IGPSportiGS630Coordinator;
+import nodomain.freeyourgadget.gadgetbridge.devices.igpsport.IGPSportiGS800Coordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.itag.ITagCoordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.jyou.BFH16DeviceCoordinator;
 import nodomain.freeyourgadget.gadgetbridge.devices.jyou.TeclastH30.TeclastH30Coordinator;
@@ -945,6 +946,7 @@ public enum DeviceType {
     BRAUN_BPW4500(BraunBPW4500DeviceCoordinator.class),
     IGPSPORT_BSC200(IGPSportBSC200Coordinator.class),
     IGPSPORT_IGS630(IGPSportiGS630Coordinator.class),
+    IGPSPORT_IGS800(IGPSportiGS800Coordinator.class),
     TEST(TestDeviceCoordinator.class);
 
     private DeviceCoordinator coordinator;
