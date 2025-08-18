6dbdd4a044e49fe039758c4e21cac571772b6811
José Rebelo <joserebelo@outlook.com>
2025-08-18 20:13:12 +0100

    Garmin Venu X1: Enable new sync protocol by default

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/garmin/watches/venu/GarminVenuX1Coordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/garmin/watches/venu/GarminVenuX1Coordinator.java
@@ -1,11 +1,32 @@
 package nodomain.freeyourgadget.gadgetbridge.devices.garmin.watches.venu;
 
+import android.content.SharedPreferences;
+
 import java.util.regex.Pattern;
 
+import nodomain.freeyourgadget.gadgetbridge.GBApplication;
 import nodomain.freeyourgadget.gadgetbridge.R;
 import nodomain.freeyourgadget.gadgetbridge.devices.garmin.watches.GarminWatchCoordinator;
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDeviceCandidate;
+import nodomain.freeyourgadget.gadgetbridge.model.DeviceType;
+import nodomain.freeyourgadget.gadgetbridge.util.preferences.DevicePrefs;
 
 public class GarminVenuX1Coordinator extends GarminWatchCoordinator {
+    @Override
+    public GBDevice createDevice(final GBDeviceCandidate candidate, final DeviceType deviceType) {
+        final GBDevice gbDevice = super.createDevice(candidate, deviceType);
+        final DevicePrefs devicePreferences = GBApplication.getDevicePrefs(gbDevice);
+        final SharedPreferences.Editor editor = devicePreferences.getPreferences().edit();
+
+        // #5021 - Venu X1 misses a lot of files without the new sync protocol
+        editor.putBoolean("new_sync_protocol", true);
+
+        editor.apply();
+
+        return gbDevice;
+    }
+
     @Override
     public boolean isExperimental() {
         // #5021 - potential pairing issues
