69eae3527c36535c996feedac635efee5efa217a
José Rebelo <joserebelo@outlook.com>
2026-01-01 16:21:11 +0000

    Ear Fun: Switch to BTBR support class

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/earfun/EarFunDeviceSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/earfun/EarFunDeviceSupport.java
@@ -1,38 +1,29 @@
 package nodomain.freeyourgadget.gadgetbridge.service.devices.earfun;
 
-import nodomain.freeyourgadget.gadgetbridge.service.AbstractHeadphoneSerialDeviceSupport;
-import nodomain.freeyourgadget.gadgetbridge.service.serial.GBDeviceIoThread;
-import nodomain.freeyourgadget.gadgetbridge.service.serial.GBDeviceProtocol;
+import java.util.UUID;
 
-public class EarFunDeviceSupport extends AbstractHeadphoneSerialDeviceSupport {
-    @Override
-    public void onSendConfiguration(String config) {
-        super.onSendConfiguration(config);
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
+import nodomain.freeyourgadget.gadgetbridge.service.btbr.TransactionBuilder;
+import nodomain.freeyourgadget.gadgetbridge.service.serial.AbstractHeadphoneSerialDeviceSupportV2;
+
+public class EarFunDeviceSupport extends AbstractHeadphoneSerialDeviceSupportV2<EarFunProtocol> {
+    public static final UUID GAIA_SPP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static final UUID GAIA_UUID = UUID.fromString("00001107-D102-11E1-9B23-00025B00A5A5");
+
+    public EarFunDeviceSupport() {
+        addSupportedService(GAIA_SPP_UUID);
     }
 
     @Override
-    public void onTestNewFunction() {
-        super.onTestNewFunction();
-    }
-
-    @Override
-    public synchronized EarFunIOThread getDeviceIOThread() {
-        return (EarFunIOThread) super.getDeviceIOThread();
-    }
-
-    @Override
-    public boolean useAutoConnect() {
-        return false;
-    }
-
-    @Override
-    protected GBDeviceProtocol createDeviceProtocol() {
+    protected EarFunProtocol createDeviceProtocol() {
         return new EarFunProtocol(getDevice());
     }
 
     @Override
-    protected GBDeviceIoThread createDeviceIOThread() {
-        return new EarFunIOThread(getDevice(), getContext(), (EarFunProtocol) getDeviceProtocol(),
-                EarFunDeviceSupport.this, getBluetoothAdapter());
+    protected TransactionBuilder initializeDevice(final TransactionBuilder builder) {
+        builder.setDeviceState(GBDevice.State.INITIALIZED);
+        builder.write(mDeviceProtocol.encodeFirmwareVersionReq());
+        builder.write(mDeviceProtocol.encodeSettingsReq());
+        return builder;
     }
 }
--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/earfun/airs/EarFunAirSDeviceSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/earfun/airs/EarFunAirSDeviceSupport.java
@@ -1,12 +1,11 @@
 package nodomain.freeyourgadget.gadgetbridge.service.devices.earfun.airs;
 
 import nodomain.freeyourgadget.gadgetbridge.service.devices.earfun.EarFunDeviceSupport;
-import nodomain.freeyourgadget.gadgetbridge.service.serial.GBDeviceProtocol;
 
 public class EarFunAirSDeviceSupport extends EarFunDeviceSupport {
 
     @Override
-    protected GBDeviceProtocol createDeviceProtocol() {
+    protected EarFunAirSProtocol createDeviceProtocol() {
         return new EarFunAirSProtocol(getDevice());
     }
 }
