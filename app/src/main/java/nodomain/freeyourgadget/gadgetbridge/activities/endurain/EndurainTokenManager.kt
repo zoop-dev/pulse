6abdb92c3c22fea1f08a50c77ab42674e9e08ab3
Arjan Schrijver <a_gadgetbridge@anymore.nl>
2026-01-18 22:41:35 +0100

    Endurain: Functional local login implementation

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/endurain/EndurainTokenManager.kt
@@ -0,0 +1,55 @@
+/*  Copyright (C) 2026 Arjan Schrijver
+
+    This file is part of Gadgetbridge.
+
+    Gadgetbridge is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gadgetbridge is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+package nodomain.freeyourgadget.gadgetbridge.activities.endurain
+
+import android.content.Context
+import androidx.core.content.edit
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+
+class EndurainTokenManager(context: Context) {
+    private val masterKey = MasterKey.Builder(context)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+
+    private val sharedPreferences = EncryptedSharedPreferences.create(
+        context,
+        "endurain_tokens",
+        masterKey,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+
+    fun saveTokens(accessToken: String, refreshToken: String) {
+        sharedPreferences.edit {
+            putString("access_token", accessToken)
+                .putString("refresh_token", refreshToken)
+                .putLong("expires_at", System.currentTimeMillis() + (15 * 60 * 1000))
+        }
+    }
+
+    fun getAccessToken(): String? = sharedPreferences.getString("access_token", null)
+    fun getRefreshToken(): String? = sharedPreferences.getString("refresh_token", null)
+    fun isTokenExpired(): Boolean {
+        val expiresAt = sharedPreferences.getLong("expires_at", 0)
+        return System.currentTimeMillis() >= expiresAt
+    }
+
+    fun clearTokens() {
+        sharedPreferences.edit { clear() }
+    }
+}
