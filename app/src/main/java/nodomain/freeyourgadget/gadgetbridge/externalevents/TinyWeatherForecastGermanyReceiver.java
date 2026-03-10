85ce4c2148cc661a4ee8d39612d2e55fbbcaf29c
José Rebelo <joserebelo@outlook.com>
2026-03-10 19:47:14 +0000

    Attempt to fix TinyWeatherForecastGermanyReceiver on some devices

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/externalevents/TinyWeatherForecastGermanyReceiver.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/externalevents/TinyWeatherForecastGermanyReceiver.java
@@ -20,8 +20,12 @@ package nodomain.freeyourgadget.gadgetbridge.externalevents;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -32,22 +36,49 @@ import nodomain.freeyourgadget.gadgetbridge.model.WeatherSpec;
 import nodomain.freeyourgadget.gadgetbridge.util.GB;
 
 public class TinyWeatherForecastGermanyReceiver extends BroadcastReceiver {
+    private static final Logger LOG = LoggerFactory.getLogger(TinyWeatherForecastGermanyReceiver.class);
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent != null) {
-            Bundle bundle = intent.getExtras();
-            if (bundle != null) {
+        if (intent == null) {
+            LOG.warn("Got null intent");
+            return;
+        }
+        final Bundle bundle = intent.getExtras();
+        if (bundle == null) {
+            LOG.warn("Got null bundle");
+            return;
+        }
+
+        LOG.debug("Got intent for {}", intent.getAction());
+
+        try {
+            bundle.setClassLoader(WeatherSpec.class.getClassLoader());
+
+            final WeatherSpec weatherSpec;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                weatherSpec = bundle.getParcelable("WeatherSpec", WeatherSpec.class);
+            } else {
+                weatherSpec = bundle.getParcelable("WeatherSpec");
+            }
+
+            if (weatherSpec == null) {
+                LOG.warn("Got null WeatherSpec");
+                return;
+            }
+
+            final ArrayList<WeatherSpec> weatherSpecs = new ArrayList<>(Collections.singletonList(weatherSpec));
+            weatherSpec.setTimestamp((int) (System.currentTimeMillis() / 1000));
+            Weather.setWeatherSpec(weatherSpecs);
+            GBApplication.deviceService().onSendWeather();
+        } catch (Exception e) {
+            GB.toast("Gadgetbridge received broken or incompatible weather data", Toast.LENGTH_SHORT, GB.ERROR, e);
+
+            for (String key : bundle.keySet()) {
                 try {
-                    WeatherSpec weatherSpec = bundle.getParcelable("WeatherSpec");
-                    if (weatherSpec != null) {
-                        ArrayList<WeatherSpec> weatherSpecs = new ArrayList<>(Collections.singletonList(weatherSpec));
-                        weatherSpec.setTimestamp((int) (System.currentTimeMillis() / 1000));
-                        Weather.setWeatherSpec(weatherSpecs);
-                        GBApplication.deviceService().onSendWeather();
-                    }
-                } catch (Exception e) {
-                    GB.toast("Gadgetbridge received broken or incompatible weather data", Toast.LENGTH_SHORT, GB.ERROR, e);
+                    LOG.debug("WeatherSpec {} -> {}", key, bundle.get(key));
+                } catch (final Exception e2) {
+                    LOG.warn("Failed to log WeatherSpec key {}", key, e2);
                 }
             }
         }
