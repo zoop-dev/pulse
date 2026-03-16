6c5332218b9a43cdf513386f95bb36d66e09180c
Thomas Kuehne <thomas.kuehne@gmx.li>
2026-03-16 21:05:49 +0100

    Garmin: fuzzy test for CobsCoDec

--- app/src/test/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/communicator/CobsCoDecTest.java
+++ app/src/test/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/communicator/CobsCoDecTest.java
@@ -1,7 +1,29 @@
+/*  Copyright (C) 2024-2026 José Rebelo, Thomas Kuehne
+
+    This file is part of Gadgetbridge.
+
+    Gadgetbridge is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gadgetbridge is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 package nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.communicator;
+
+import static nodomain.freeyourgadget.gadgetbridge.service.btle.AbstractBTLEDeviceSupport.calcMaxWriteChunk;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Random;
 
 import nodomain.freeyourgadget.gadgetbridge.util.GB;
 
@@ -90,7 +112,12 @@ public class CobsCoDecTest {
         final byte[] test_string_7 = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, -128, -127, -126, -125, -124, -123, -122, -121, -120, -119, -118, -117, -116, -115, -114, -113, -112, -111, -110, -109, -108, -107, -106, -105, -104, -103, -102, -101, -100, -99, -98, -97, -96, -95, -94, -93, -92, -91, -90, -89, -88, -87, -86, -85, -84, -83, -82, -81, -80, -79, -78, -77, -76, -75, -74, -73, -72, -71, -70, -69, -68, -67, -66, -65, -64, -63, -62, -61, -60, -59, -58, -57, -56, -55, -54, -53, -52, -51, -50, -49, -48, -47, -46, -45, -44, -43, -42, -41, -40, -39, -38, -37, -36, -35, -34, -33, -32, -31, -30, -29, -28, -27, -26, -25, -24, -23, -22, -21, -20, -19, -18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1};
         final byte[] test_string_8 = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, -128, -127, -126, -125, -124, -123, -122, -121, -120, -119, -118, -117, -116, -115, -114, -113, -112, -111, -110, -109, -108, -107, -106, -105, -104, -103, -102, -101, -100, -99, -98, -97, -96, -95, -94, -93, -92, -91, -90, -89, -88, -87, -86, -85, -84, -83, -82, -81, -80, -79, -78, -77, -76, -75, -74, -73, -72, -71, -70, -69, -68, -67, -66, -65, -64, -63, -62, -61, -60, -59, -58, -57, -56, -55, -54, -53, -52, -51, -50, -49, -48, -47, -46, -45, -44, -43, -42, -41, -40, -39, -38, -37, -36, -35, -34, -33, -32, -31, -30, -29, -28, -27, -26, -25, -24, -23, -22, -21, -20, -19, -18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, -128, -127, -126, -125, -124, -123, -122, -121, -120, -119, -118, -117, -116, -115, -114, -113, -112, -111, -110, -109, -108, -107, -106, -105, -104, -103, -102, -101, -100, -99, -98, -97, -96, -95, -94, -93, -92, -91, -90, -89, -88, -87, -86, -85, -84, -83, -82, -81, -80, -79, -78, -77, -76, -75, -74, -73, -72, -71, -70, -69, -68, -67, -66, -65, -64, -63, -62, -61, -60, -59, -58, -57, -56, -55, -54, -53, -52, -51, -50, -49, -48, -47, -46, -45, -44, -43, -42, -41, -40, -39, -38, -37, -36, -35, -34, -33, -32, -31, -30, -29, -28, -27, -26, -25, -24, -23, -22, -21, -20, -19, -18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, 0, -5, -4, -3, -2, -1};
         final byte[] test_string_long_nonzero = GB.hexStringToByteArray("49535a73394d73483159456f515352443069546e466c4b32394d3479336f396a6936543263544954794e576b456f4e365769704734444a6b4c6439657a6256386b30676f4e544e587954326e596e617567334f546449376c535471506e724e4444326664774d7a3142725653596c4838794b524b50327a5a3438704961545457384b483571744a6c726948704552364b7a466c54315776337a373954524942467442784c3062486f6c386b786a48377750726f5277766546757a596876533731726e6972344e644f70475a6c6a4c65753371554545396a4f556974703655774b426b34575970754e4f484b6349364f425468334c753532324b66abababab000B");
+        //test strings from #5903
+        final byte[] test_string_9 = {1, 0, 0};
+        final byte[] test_string_10 = {1};
+
         final byte[][] allTests = new byte[][]{
+                test_string_0,
                 test_string_1,
                 test_string_2,
                 test_string_3,
@@ -100,6 +127,8 @@ public class CobsCoDecTest {
                 test_string_7,
                 test_string_8,
                 test_string_long_nonzero,
+                test_string_9,
+                test_string_10,
         };
 
         for (byte[] payload : allTests) {
@@ -109,5 +138,92 @@ public class CobsCoDecTest {
 
             Assert.assertArrayEquals(payload, decodedData);
         }
+    }
+
+    // reproducible fuzzing
+    @Test
+    public void TestEncodeDecodeRoundTrip() {
+        Random random = new Random(192837465L);
+
+        // general fuzzing
+        for (int round = 0; round < 100_000; round++) {
+            // lower bound due to minimal GFDI overhead
+            // upper bound due to capacity of CobsCoDec's byteBuffer
+            byte[] payload = new byte[random.nextInt(4, 10_000 / 2 - 2)];
+
+            // "random" payload
+            random.nextBytes(payload);
+
+            // minimum MTU
+            chunkedEncodeDecode(payload, 23);
+
+            // maximum MTU
+            chunkedEncodeDecode(payload, 517);
+
+            // "random" MTU
+            int mtu = random.nextInt(23, 517);
+            chunkedEncodeDecode(payload, mtu);
+        }
+
+
+        // small payload length
+        for (int len = 1; len <= 517; len++) {
+            byte[] payload = new byte[len];
+            random.nextBytes(payload);
+            chunkedEncodeDecode(payload, 517);
+        }
+
+        // MTU range
+        for (int mtu = 23; mtu <= 517; mtu++) {
+            // upper bound due to capacity of CobsCoDec's byteBuffer
+            byte[] payload = new byte[random.nextInt(mtu + 1, 10_000 / 2 - 2)];
+            random.nextBytes(payload);
+            chunkedEncodeDecode(payload, mtu);
+        }
+
+        // systematic exploration at the boundaries
+        for (int len : new int[]{1, 2, 3, 0xFE, 0xFF, 0x100}) {
+            byte[] bytes = new byte[len];
+            random.nextBytes(bytes);
+
+            // all combinations for last two values
+            for (int x = Byte.MIN_VALUE; x <= Byte.MAX_VALUE; x++) {
+                bytes[len - 1] = (byte) x;
+                if (len > 1) {
+                    for (int y = Byte.MIN_VALUE; y <= Byte.MAX_VALUE; y++) {
+                        bytes[len - 2] = (byte) y;
+                        chunkedEncodeDecode(bytes, random.nextInt(23, 517));
+                    }
+                } else {
+                    chunkedEncodeDecode(bytes, 23);
+                }
+            }
+
+            // all the same value
+            for (int every = Byte.MIN_VALUE; every <= Byte.MAX_VALUE; every++) {
+                Arrays.fill(bytes, (byte) every);
+                chunkedEncodeDecode(bytes, 517);
+            }
+        }
+    }
+
+    private void chunkedEncodeDecode(final byte[] payload, final int mtu) {
+        int maxChunkSize = calcMaxWriteChunk(mtu);
+        byte[] encodedData = CobsCoDec.encode(payload);
+
+        int end;
+        for (int start = 0; true; start = end) {
+            end = start + maxChunkSize;
+            if (end >= encodedData.length) {
+                end = encodedData.length;
+            }
+            if (end <= start) {
+                break;
+            }
+            byte[] chunk = java.util.Arrays.copyOfRange(encodedData, start, end);
+            cobsCoDec.receivedBytes(chunk);
+        }
+        byte[] decodedData = cobsCoDec.retrieveMessage();
+        Assert.assertArrayEquals(payload, decodedData);
     }
 }
