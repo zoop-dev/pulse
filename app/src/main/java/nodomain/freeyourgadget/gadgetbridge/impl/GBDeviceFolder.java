e79ac44861ff405a6b7316273672b1e87819ff18
Thomas Kuehne <thomas.kuehne@gmx.li>
2026-05-31 15:44:31 +0000

    GBDevice: code review

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/impl/GBDeviceFolder.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/impl/GBDeviceFolder.java
@@ -16,9 +16,7 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 package nodomain.freeyourgadget.gadgetbridge.impl;
 
-import nodomain.freeyourgadget.gadgetbridge.model.DeviceType;
-
-public class GBDeviceFolder extends GBDevice{
+public class GBDeviceFolder extends GBDevice {
     public GBDeviceFolder(String name) {
         super("", name, null, null, null);
     }
