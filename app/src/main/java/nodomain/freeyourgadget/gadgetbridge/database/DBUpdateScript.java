78496dbb3aafefb6fec501091f8281694d3a177b
Thomas Kuehne <thomas.kuehne@gmx.li>
2026-06-01 03:43:27 +0000

    DBUpdateScript: add @Nullable / @NonNull annotations

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/database/DBUpdateScript.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/database/DBUpdateScript.java
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2015-2024 Andreas Shimokawa, Carsten Pfeiffer
+/*  Copyright (C) 2015-2026 Andreas Shimokawa, Carsten Pfeiffer, Thomas Kuehne
 
     This file is part of Gadgetbridge.
 
@@ -18,16 +18,19 @@ package nodomain.freeyourgadget.gadgetbridge.database;
 
 import android.database.sqlite.SQLiteDatabase;
 
+import androidx.annotation.NonNull;
+
 /**
  * Interface for updating a database schema.
  * Implementors provide the update from the prior schema
  * version to this version, and the downgrade from this schema
  * version to the next lower version.
- * <p/>
+ * <p>
  * Implementations must have a public, no-arg constructor.
+ * </p>
  */
 public interface DBUpdateScript {
-    void upgradeSchema(SQLiteDatabase database);
+    void upgradeSchema(@NonNull SQLiteDatabase database);
 
-    void downgradeSchema(SQLiteDatabase database);
+    void downgradeSchema(@NonNull SQLiteDatabase database);
 }
