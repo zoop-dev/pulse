31b7739fafc20b2898a48de00f9e484fb2978500
Andreas Shimokawa <shimokawa@fsfe.org>
2026-07-07 12:39:57 +0200

    Pebble: fix icons for TrekVolleV3Reworked

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/pebble/AppMessageHandlerTrekV3Reworked.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/pebble/AppMessageHandlerTrekV3Reworked.java
@@ -33,11 +33,25 @@ import nodomain.freeyourgadget.gadgetbridge.model.weather.Weather;
 import nodomain.freeyourgadget.gadgetbridge.util.GB;
 
 class AppMessageHandlerTrekV3Reworked extends AppMessageHandler {
-    private static final int ID_IMAGE_ERROR = 14;
     private int MESSAGE_KEY_WEATHER_TEMPERATURE;
     private int MESSAGE_KEY_WEATHER_ICON;
     private int MESSAGE_KEY_WEATHER_TEMP_RANGE;
 
+    private static final int ID_WEATHER_CLEAR_DAY = 0;
+    private static final int ID_WEATHER_CLEAR_NIGHT = 1;
+    private static final int ID_WEATHER_WINDY = 2;
+    private static final int ID_WEATHER_COLD = 3;
+    private static final int ID_WEATHER_PARTLY_CLOUDY_DAY = 4;
+    private static final int ID_WEATHER_PARTLY_CLOUDY_NIGHT = 5;
+    private static final int ID_WEATHER_HAZE = 6;
+    private static final int ID_WEATHER_CLOUD = 7;
+    private static final int ID_WEATHER_RAIN = 8;
+    private static final int ID_WEATHER_SNOW = 9;
+    private static final int ID_WEATHER_HAIL = 10;
+    private static final int ID_WEATHER_CLOUDY = 11;
+    private static final int ID_WEATHER_STORM = 12;
+    private static final int ID_WEATHER_FOG = 13;
+    private static final int ID_WEATHER_NA = 14;
     AppMessageHandlerTrekV3Reworked(UUID uuid, PebbleProtocol pebbleProtocol) {
         super(uuid, pebbleProtocol);
 
@@ -52,15 +66,52 @@ class AppMessageHandlerTrekV3Reworked extends AppMessageHandler {
         }
     }
 
-    private int getIconForConditionCode(int code) {
-        // from javascript
-        if (code >= 200 && code <= 232) return 0;
-        if (code >= 300 && code <= 531) return 11;
-        if (code >= 600 && code <= 622) return 13;
-        if (code >= 700 && code <= 781) return 20;
-        if (code >= 801 && code <= 804) return 26;
-        if (code == 800) return 36;
-        return ID_IMAGE_ERROR;
+    private int getIconForConditionCode(int conditionCode, boolean isNight) {
+        if (conditionCode == 800 || conditionCode == 951) {
+            return isNight ? ID_WEATHER_CLEAR_NIGHT : ID_WEATHER_CLEAR_DAY;
+        } else if (conditionCode == 801 || conditionCode == 802) {
+            return isNight ? ID_WEATHER_PARTLY_CLOUDY_NIGHT : ID_WEATHER_PARTLY_CLOUDY_DAY;
+        } else if (conditionCode >= 300 && conditionCode < 313) {
+            return ID_WEATHER_RAIN;
+        } else if ((conditionCode >= 313 && conditionCode < 400) || conditionCode == 500) {
+            return ID_WEATHER_RAIN;
+        } else if (conditionCode >= 500 && conditionCode < 600) {
+            return ID_WEATHER_RAIN;
+        } else if (conditionCode >= 700 && conditionCode < 732) {
+            return ID_WEATHER_HAZE;
+        } else if (conditionCode == 741 || conditionCode == 751 || conditionCode == 761 || conditionCode == 762) {
+            return ID_WEATHER_FOG;
+        } else if (conditionCode == 771) {
+            return ID_WEATHER_WINDY;
+        } else if (conditionCode == 781) {
+            return ID_WEATHER_STORM;
+        } else if (conditionCode >= 200 && conditionCode < 300) {
+            return ID_WEATHER_STORM;
+        } else if (conditionCode == 600 || conditionCode == 601 || conditionCode == 602) {
+            return ID_WEATHER_SNOW;
+        } else if (conditionCode == 611 || conditionCode == 612) {
+            return ID_WEATHER_HAIL;
+        } else if (conditionCode == 615 || conditionCode == 616 || conditionCode == 620 || conditionCode == 621 || conditionCode == 622) {
+            return ID_WEATHER_SNOW;
+        } else if (conditionCode == 906) {
+            return ID_WEATHER_SNOW;
+        } else if (conditionCode == 803) {
+            return ID_WEATHER_CLOUDY;
+        } else if (conditionCode == 804) {
+            return ID_WEATHER_CLOUD;
+        } else if (conditionCode >= 907 && conditionCode < 957) {
+            return ID_WEATHER_STORM;
+        } else if (conditionCode == 905) {
+            return ID_WEATHER_STORM;
+        } else if (conditionCode == 900) {
+            return ID_WEATHER_STORM;
+        } else if (conditionCode == 901 || conditionCode == 902 || conditionCode == 962) {
+            return ID_WEATHER_STORM;
+        } else if (conditionCode == 903) {
+            return ID_WEATHER_COLD;
+        }
+
+        return ID_WEATHER_NA;
     }
 
     private byte[] encodeTrekV3ReworkedWeather(WeatherSpec weatherSpec) {
@@ -69,9 +120,12 @@ class AppMessageHandlerTrekV3Reworked extends AppMessageHandler {
             return null;
         }
 
-
+        boolean isNight = false;
+        if (weatherSpec.getSunRise() != 0 && weatherSpec.getSunSet() != 0) {
+            isNight = weatherSpec.getSunRise() * 1000L > System.currentTimeMillis() || weatherSpec.getSunSet() * 1000L < System.currentTimeMillis();
+        }
         ArrayList<Pair<Integer, Object>> pairs = new ArrayList<>();
-        pairs.add(new Pair<>(MESSAGE_KEY_WEATHER_ICON, getIconForConditionCode(weatherSpec.getCurrentConditionCode())));
+        pairs.add(new Pair<>(MESSAGE_KEY_WEATHER_ICON, getIconForConditionCode(weatherSpec.getCurrentConditionCode(), isNight)));
         pairs.add(new Pair<>(MESSAGE_KEY_WEATHER_TEMPERATURE, (weatherSpec.getCurrentTemp() - 273) + "°"));
         pairs.add(new Pair<>(MESSAGE_KEY_WEATHER_TEMP_RANGE, (weatherSpec.getTodayMinTemp() - 273) + "° " + (weatherSpec.getTodayMaxTemp() - 273) + "°"));
 
