f4378bd682d8694558e5911d1a814ebee1c381c6
José Rebelo <joserebelo@outlook.com>
2026-08-14 11:54:29 +0100

    Garmin: Add new oauth token urls
    As seen on a btsnoop from an Enduro 3

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/http/interceptors/OauthInterceptor.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/http/interceptors/OauthInterceptor.java
@@ -16,8 +16,9 @@ import org.slf4j.LoggerFactory;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -46,7 +47,9 @@ public class OauthInterceptor implements HttpInterceptor {
 
     @Override
     public boolean supports(@NonNull final GarminHttpRequest request) {
-        return request.getPath().startsWith("/api/oauth") || request.getPath().startsWith("/oauthTokenExchangeService");
+        return request.getPath().startsWith("/api/oauth/") ||
+                request.getPath().startsWith("/oauth/") ||
+                request.getPath().startsWith("/oauthTokenExchangeService/");
     }
 
     @Override
@@ -60,13 +63,15 @@ public class OauthInterceptor implements HttpInterceptor {
         final GarminPrefs devicePrefs = deviceSupport.getDevicePrefs();
         if (!devicePrefs.fakeOauthEnabled()) {
             LOG.warn("Got OAuth HTTP request, but fake OAuth is disabled");
-            if (request.getPath().equals("/oauthTokenExchangeService/connectToIT") || request.getPath().equals("/api/oauth/token")) {
+            if (request.getPath().equals("/oauthTokenExchangeService/connectToIT") ||
+                    request.getPath().equals("/oauth/refresh_token/token") ||
+                    request.getPath().equals("/api/oauth/token")) {
                 showAuthExpiredNotification();
             }
             return null;
         }
 
-        final List<String> scopes = Arrays.asList(
+        final Set<String> scopes = new LinkedHashSet<>(Arrays.asList(
                 // Swim 2
                 "GCS_EPHEMERIS_SONY_READ",
                 // Venu 3
@@ -78,10 +83,43 @@ public class OauthInterceptor implements HttpInterceptor {
                 "GCS_LIVETRACK_FIT_READ",
                 "GCS_LIVETRACK_FIT_UPDATE",
                 "OMT_GOLF_SUBSCRIPTION_READ",
-                "OMT_SUBSCRIPTION_READ"
-        );
+                "OMT_SUBSCRIPTION_READ",
+                // Enduro 3
+                "CSE_CDS_ACCOUNT_READ",
+                "GCS_CIQ_APPSTORE_MOBILE_READ",
+                "GCS_DEVICE_INSTRUCTION_OUTDOOR_READ",
+                "GCS_EMERGENCY_ASSISTANCE_CREATE",
+                "GCS_EPHEMERIS_SONY_READ",
+                "GCS_GEOLOCATION_ELEVATION_READ",
+                "GCS_IMAGE_STORAGE_READ",
+                "GCS_LIVETRACK_FIT_CREATE",
+                "GCS_LIVETRACK_FIT_READ",
+                "GCS_LIVETRACK_FIT_UPDATE",
+                "GCS_LIVE_EVENT_SHARING_CREATE",
+                "GCS_LTE_SIGNAL_UPDATE",
+                "GCS_MESSAGING_FITNESS_CREATE",
+                "GCS_MESSAGING_FITNESS_READ",
+                "GCS_MESSAGING_FITNESS_UPDATE",
+                "GCS_STOCKS_READ",
+                "GCS_TIDE_READ",
+                "GCS_WEATHER_RACEDAY_READ",
+                "MARINE_SERVER_ACCESS",
+                "MARINE_SERVER_CHARTS_SUBSCRIPTION_READ",
+                "MARINE_SERVER_CHARTS_SUBSCRIPTION_WRITE",
+                "OMT_BIRDSEYE_READ",
+                "OMT_OUTDOOR_MAP_SUBSCRIPTION_CREATE",
+                "OMT_OUTDOOR_MAP_SUBSCRIPTION_DELETE",
+                "OMT_OUTDOOR_MAP_SUBSCRIPTION_READ",
+                "OMT_SUBSCRIPTION_READ",
+                "YAR_BILLING_SUBSCRIBER_READ",
+                "YAR_INREACH_HERMES_READ",
+                "YAR_INREACH_IRIS_CREATE",
+                "YAR_INREACH_IRIS_READ",
+                "YAR_INREACH_IRIS_UPDATE",
+                "YAR_INREACH_VOICE_EVENT_CREATE"
+        ));
 
-        if (request.getPath().equals("/oauthTokenExchangeService/connectToIT")) {
+        if (request.getPath().equals("/oauthTokenExchangeService/connectToIT") || request.getPath().equals("/oauth/connect_exchange/token")) {
             final AuthorizationResponse authorizationResponse = new AuthorizationResponse();
             authorizationResponse.accessToken = UUID.randomUUID().toString();
             authorizationResponse.tokenType = "Bearer";
@@ -96,7 +134,7 @@ public class OauthInterceptor implements HttpInterceptor {
             response.setBody(GSON.toJson(authorizationResponse).getBytes(StandardCharsets.UTF_8));
             response.getHeaders().put("Content-Type", "application/json");
             return response;
-        } else if (request.getPath().equals("/api/oauth/token")) {
+        } else if (request.getPath().equals("/api/oauth/token") || request.getPath().equals("/oauth/refresh_token/token")) {
             // Attempt to keep the same refresh token
             final String refreshToken;
             if (request.getRawRequest().hasRawBody()) {
