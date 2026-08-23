cf9271a9586640f4ea71aef58a40cb52c7671253
Thomas Kuehne <thomas.kuehne@gmx.li>
2026-08-23 09:56:04 +0000

    Garmin: improve error handling when decoding/encoding DayOfWeek FIT fields

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/fit/fieldDefinitions/FieldDefinitionDayOfWeek.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/garmin/fit/fieldDefinitions/FieldDefinitionDayOfWeek.java
@@ -1,24 +1,28 @@
 package nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.fit.fieldDefinitions;
 
+import androidx.annotation.Nullable;
+
 import java.nio.ByteBuffer;
 import java.time.DayOfWeek;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.fit.FieldDefinition;
 import nodomain.freeyourgadget.gadgetbridge.service.devices.garmin.fit.baseTypes.BaseType;
 
 public class FieldDefinitionDayOfWeek extends FieldDefinition {
 
-    public FieldDefinitionDayOfWeek(int localNumber, int size, BaseType baseType, String name) {
-        super(localNumber, size, baseType, name, 1, 0);
+    public FieldDefinitionDayOfWeek(int localNumber, int size, BaseType baseType, String name, int scale, int offset) {
+        super(localNumber, size, baseType, name, scale, offset);
     }
 
+    @Nullable
     @Override
     public Object decode(ByteBuffer byteBuffer) {
         final Object rawObj = baseType.decode(byteBuffer, scale, offset);
-        if (rawObj != null) {
-            final int raw = (int) rawObj;
+        if (rawObj instanceof final Number number) {
+            final int raw = number.intValue();
             return DayOfWeek.of(raw == 0 ? 7 : raw);
         }
         return null;
@@ -26,14 +30,20 @@ public class FieldDefinitionDayOfWeek extends FieldDefinition {
 
     @Override
     public void encode(ByteBuffer byteBuffer, Object o) {
-        if (o == null) {
-            baseType.encode(byteBuffer, null, scale, offset);
-            return;
+        if (o instanceof final Number number) {
+            long epoc = number.longValue();
+            Instant instant = Instant.ofEpochSecond(epoc);
+            ZoneId zoneId = ZoneId.systemDefault();
+            ZonedDateTime zonedDateTime = instant.atZone(zoneId);
+            o = zonedDateTime.getDayOfWeek();
         }
-        if (o instanceof DayOfWeek) {
-            baseType.encode(byteBuffer, (((DayOfWeek) o).getValue() % 7), scale, offset);
-            return;
+
+        if (o instanceof final DayOfWeek dayOfWeek) {
+            o = dayOfWeek.getValue() % 7;
+        } else {
+            o = null;
         }
-        baseType.encode(byteBuffer, (Instant.ofEpochSecond((int) o).atZone(ZoneId.systemDefault()).getDayOfWeek().getValue() % 7), scale, offset);
+
+        baseType.encode(byteBuffer, o, scale, offset);
     }
 }
